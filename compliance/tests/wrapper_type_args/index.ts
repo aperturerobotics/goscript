b34b24327c8f8eff43168bb37defe5271f31367a
Christian Stewart <christian@aperture.us>
2025-07-11 16:39:09 -0700

    fix: detect if wrapper type receiver has a pointer type and unwrap
    See #91 and 4e3fc0 and 142201
    
    ```
    go test -timeout 30s -run ^TestCompliance/wrapper_slice_append$ ./compiler
    ```
    
    Fixes: #96
    
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- compliance/tests/wrapper_type_args/index.ts
+++ compliance/tests/wrapper_type_args/index.ts
@@ -1,3 +1,3 @@
-export { TestFileMode, TestMyMode } from "./wrapper_type_args.gs.js"
+export { MyMode_IsExecutable, MyMode_String, TestFileMode, TestMyMode } from "./wrapper_type_args.gs.js"
 export { MyDir } from "./wrapper_type_args.gs.js"
 export type { DirInterface, MyMode } from "./wrapper_type_args.gs.js"
