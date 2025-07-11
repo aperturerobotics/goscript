df619d5c7ac7872ef2b135051616a4aaf9d699ad
Randall C. O'Reilly <oreilly@ucdavis.edu>
2025-07-10 17:23:35 -0700

    add import_type_methods test, which reproduces issue from #89 on importing methods defined on a slice type (but struct type works fine)

--- compliance/tests/import_type_methods/errlist/errlist.go
+++ compliance/tests/import_type_methods/errlist/errlist.go
@@ -5,3 +5,11 @@ type ErrorList []string
 func (p *ErrorList) Add(msg string) {
 	*p = append(*p, msg)
 }
+
+type AStruct struct {
+	Msg string
+}
+
+func (a *AStruct) Set(msg string) {
+	a.Msg = msg
+}
---
+++ compliance/tests/import_type_methods/errlist/index.ts
@@ -0,0 +1,2 @@
+export { AStruct } from "./errlist.gs.js"
+export type { ErrorList } from "./errlist.gs.js"
