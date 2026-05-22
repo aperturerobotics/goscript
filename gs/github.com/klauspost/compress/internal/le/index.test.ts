a8335154109ba3fcf43d4c728d496bcdc1842bc2
Christian Stewart <christian@aperture.us>
2026-05-22 09:37:13 -0700

    test(overrides): use vitest for s2 le coverage
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- gs/github.com/klauspost/compress/internal/le/index.test.ts
+++ gs/github.com/klauspost/compress/internal/le/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'bun:test'
+import { describe, expect, it } from 'vitest'
 
 import { arrayToSlice } from '@goscript/builtin/index.js'
 import { TrailingZeros64 } from '@goscript/math/bits/index.js'
