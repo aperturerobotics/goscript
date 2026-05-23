e89c824f66b1c4a6537b1092a6147d9322363939
Christian Stewart <christian@aperture.us>
2026-05-22 18:03:44 -0700

    test(crypto): use vitest for sha256 override
    Signed-off-by: Christian Stewart <christian@aperture.us>

--- gs/crypto/sha256/index.test.ts
+++ gs/crypto/sha256/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'bun:test'
+import { describe, expect, test } from 'vitest'
 import * as $ from '@goscript/builtin/index.js'
 
 import { New, Size, Sum256 } from './index.js'
